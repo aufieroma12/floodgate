22975373873447d706375c300d1a6234200004a1
Massimo Aufiero <aufieroma12@gmail.com>
2022-07-16 08:34:20 -0400

    relocated load_modules.sh

--- config/load_modules.sh
+++ config/load_modules.sh
@@ -2,13 +2,11 @@ module purge
 module load python/3.8.5-fasrc01
 module load Anaconda3/2020.11
 module load cuda/11.1.0-fasrc01 cudnn/8.0.4.30_cuda11.1-fasrc01
-source activate tf_gpu
 pip install --upgrade tensorflow-gpu==2.8.0
 
-TF_VERSION=$(conda list tensorflow)
+TF_VERSION=$(pip list | grep tensorflow-gpu)
 PYTHON_VERSION=$(python --version)
 CONDA_VERSION=$(conda --version)
-echo 'loaded' ${TF_VERSION:138:31}
 echo 'loaded' $PYTHON_VERSION
 echo 'loaded' $CONDA_VERSION
-
+echo 'loaded' ${TF_VERSION:0:40}
