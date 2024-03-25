c86875fa55319291a5c2c7459f47267fc2a287f0
Massimo Aufiero <massimo@robustintelligence.com>
2024-03-25 11:53:53 -0700

    use setup.py

--- Hymod/analysis/get_surrogate_batched_preds.sh
+++ Hymod/analysis/get_surrogate_batched_preds.sh
@@ -1,3 +1,3 @@
 export BATCH_SIZE=1000000
-mkdir -p ../log/analysis/n_100000_preds/
-sbatch --array=0-99 get_surrogate_batched_preds.sbatch
+mkdir -p Hymod/log/analysis/n_100000_preds/
+sbatch --array=0-99 Hymod/analysis/get_surrogate_batched_preds.sbatch
---
+++ setup.py
@@ -0,0 +1,18 @@
+"""Setup file for package."""
+from setuptools import find_packages, setup
+
+setup(
+    name="floodgate",
+    version="0.0.0",
+    packages=find_packages(include=["src*", "ishigami*", "config*", "Hymod*", "CBMZ*"]),
+    description="package for running floodgate sensitivity analysis.",
+    install_requires=[
+        "scikit-learn<=1.1.1",
+        "numpy",
+        "tensorflow",
+        "safepython",
+        "joblib",
+        "matplotlib",
+        "pandas",
+    ],
+)
