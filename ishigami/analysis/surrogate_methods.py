3bac0195e2d42236c03be76a46046f98f2e41f91
Massimo Aufiero <massimo@robustintelligence.com>
2024-03-25 03:34:49 -0700

    finish ishigami scripts

--- ishigami/analysis/surrogate_methods.py
+++ ishigami/analysis/surrogate_methods.py
@@ -3,7 +3,6 @@ import os
 from pathlib import Path
 from time import time
 
-import joblib
 import numpy as np
 
 from src.sensitivity import combined_surrogate_methods
@@ -11,95 +10,84 @@ from src.surrogate import Ishigami
 
 from config import Random_seeds, Ishigami_inputs
 
-parser = argparse.ArgumentParser()
-parser.add_argument(
-    "--train_size",
-    type=int,
-    required=True,
-    help="Size of training set.",
-)
-parser.add_argument(
-    "--num_datasets",
-    type=int,
-    required=True,
-    help="Number of datasets to evaluate.",
-)
-parser.add_argument("--index", type=int, required=True, help="Index of run.")
-parser.add_argument(
-    "--save_data",
-    type=bool,
-    required=False,
-    default=True,
-    help="Whether to save the model inputs and outputs.",
-)
-args = parser.parse_args()
-
-index = args.index
-num_datasets = args.num_datasets
-train_size = args.train_size
-
-start = index * num_datasets
-end = (index + 1) * num_datasets
-
-N = int(1e4)
+N = int(1e3)
 xmin = Ishigami_inputs["min"]
 xmax = Ishigami_inputs["max"]
 d = xmax.shape[0]
 
-MODEL_PATH = Path(__file__).parents[1] / "models" / f"n_{train_size}.pkl"
 DATA_DIR = Path(__file__).parents[1] / "data"
-
 OUTPUT_DIR = DATA_DIR / "analysis"
-FLOODGATE_OUTPUT_DIR = OUTPUT_DIR / "floodgate" / f"{train_size}"
-SPF_SURROGATE_OUTPUT_DIR = OUTPUT_DIR / "spf_surrogate" / f"{train_size}"
-PANIN_OUTPUT_DIR = OUTPUT_DIR / "panin" / f"{train_size}"
 
-os.makedirs(FLOODGATE_OUTPUT_DIR, exist_ok=True)
-os.makedirs(SPF_SURROGATE_OUTPUT_DIR, exist_ok=True)
-os.makedirs(PANIN_OUTPUT_DIR, exist_ok=True)
 
-fstar = Ishigami()
-f = joblib.load(MODEL_PATH)
+def analytical_mse(a, b, c):
+    a_diff = a - 1
+    b_diff = b - 7
+    c_diff = c - 0.1
+    unnormalized = (
+        4 * (a_diff ** 2) * (np.pi ** 3) +
+        (4 / 9) * (c_diff ** 2) * (np.pi ** 11) +
+        (8 / 5) * a_diff * c_diff * (np.pi ** 7) +
+        3 * (b_diff ** 2) * (np.pi ** 3)
+    )
+    return unnormalized / ((2 * np.pi) ** 3)
 
-for i in range(start, end):
-    print(f'Evaluating dataset {i}:')
-    data_path = DATA_DIR / f"{i}.npy"
-    if os.path.exists(data_path):
-        data = np.load(data_path)
-        X = data[:, :-1]
-        y = data[:, -1]
-        print("  Data read from file.\n")
-    else:
-        np.random.seed(Random_seeds["Ishigami_inputs"] + i)
-        X = np.random.rand(N, d) @ np.diag(xmax - xmin) + np.ones((N, d)) @ np.diag(xmin)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--noise",
+        type=float,
+        required=True,
+        help="Noise to add to the Ishigami coefficients for the surrogate.",
+    )
+    parser.add_argument(
+        "--save-data",
+        action="store_true",
+        default=False,
+        help="Whether to save the model inputs and outputs.",
+    )
+    args = parser.parse_args()
+    noise = args.noise
+
+    FLOODGATE_OUTPUT_DIR = OUTPUT_DIR / "floodgate" / f"{noise}"
+    SPF_SURROGATE_OUTPUT_DIR = OUTPUT_DIR / "spf_surrogate" / f"{noise}"
+    PANIN_OUTPUT_DIR = OUTPUT_DIR / "panin" / f"{noise}"
+
+    os.makedirs(FLOODGATE_OUTPUT_DIR, exist_ok=True)
+    os.makedirs(SPF_SURROGATE_OUTPUT_DIR, exist_ok=True)
+    os.makedirs(PANIN_OUTPUT_DIR, exist_ok=True)
+
+    fstar = Ishigami()
+    f = Ishigami(1 + noise, 7 + 2 * noise, 0.1 - 0.5 * noise)
+
+    print(f"MSE: {analytical_mse(f.a, f.b, f.c):.5f}")
+
+    for i in range(1000):
+        if i % 100 == 0:
+            print(f'Evaluating dataset {i}:')
+
+        data_path = DATA_DIR / "outputs" / f"{i}.npy"
+        os.makedirs(data_path.parent, exist_ok=True)
+        if os.path.exists(data_path):
+            data = np.load(data_path)
+            X = data[:, :-1]
+            y = data[:, -1]
+        else:
+            np.random.seed(Random_seeds["Ishigami_inputs"] + i)
+            X = np.random.rand(N, d) @ np.diag(xmax - xmin) + np.ones((N, d)) @ np.diag(xmin)
+            t1 = time()
+            y = fstar.predict(X)
+
+            if args.save_data:
+                np.save(data_path, np.concatenate((X, y.reshape(-1, 1)), axis=1))
+
+        np.random.seed(Random_seeds["Ishigami_analysis"] + i)
+
         t1 = time()
-        y = fstar.predict(X)
-        print(f"  Total model evalutations ({N}): {(time() - t1): .2f} seconds")
+        floodgate_results, spf_results, panin_results = combined_surrogate_methods(
+            X, f, xmin, xmax, Y=y
+        )
 
-        if args.save_data:
-            np.save(data_path, np.concatenate((X, y.reshape(-1, 1)), axis=1))
-
-    print(f"  Train size: {train_size}")
-    floodgate_results = []
-    spf_results = []
-    panin_results = []
-    np.random.seed(Random_seeds["Ishigami_analysis"] + i)
-
-    t1 = time()
-    flood, spf, panin = combined_surrogate_methods(X, f, xmin, xmax, Y=y)
-    print(f"  Analysis: {time() - t1: .3f} seconds")
-
-    floodgate_results.append(flood)
-    spf_results.append(spf)
-    panin_results.append(panin)
-
-    floodgate_results = np.array(floodgate_results)
-    spf_results = np.array(spf_results)
-    panin_results = np.array(panin_results)
-
-    np.save(FLOODGATE_OUTPUT_DIR / f"{i}.npy", floodgate_results)
-    np.save(SPF_SURROGATE_OUTPUT_DIR / f"{i}.npy", spf_results)
-    np.save(PANIN_OUTPUT_DIR / f"{i}.npy", panin_results)
-
-    print()
-
+        np.save(FLOODGATE_OUTPUT_DIR / f"{i}.npy", np.array(floodgate_results))
+        np.save(SPF_SURROGATE_OUTPUT_DIR / f"{i}.npy", np.array(spf_results))
+        np.save(PANIN_OUTPUT_DIR / f"{i}.npy", np.array(panin_results))
--- plotting_scripts/Ishigami.py
+++ plotting_scripts/Ishigami.py
@@ -1,23 +1,19 @@
+from pathlib import Path
+
 import numpy as np
-import pandas as pd
 from matplotlib import pyplot as plt
 import os
 
-import sys
-sys.path.append('../config/')
+from ishigami.analysis.surrogate_methods import analytical_mse
 
-from config import Hymod_inputs
+FIG_DIR = Path(__file__).parents[1] / "figs"
+DATA_DIR = Path(__file__).parents[1] / "ishigami" / "data" / "analysis_10k"
 
-
-FIG_DIR = '../figs/'
-DATA_DIR = '../Hymod/data/analysis/{}/{}/'
 if not os.path.exists(FIG_DIR):
     os.mkdir(FIG_DIR)
 
-sample_sizes = [100, 250, 500, 1000, 5000, 10000, 50000]
-X_labels = Hymod_inputs['labels']
-d = len(X_labels)
-gt = np.load(DATA_DIR.format('.','.') + 'ground_truth.npy')
+d = 3
+gt = np.array([0.5574, 0.4424, 0.2436])
 
 
 # Plot formatting
@@ -29,97 +25,80 @@ BIG_SIZE = 24
 BIGGER_SIZE = 30
 HUGE_SIZE = 32
 
-plt.rc('font', size=SMALL_SIZE)           # controls default text sizes
+plt.rc('font', size=BIG_SIZE)           # controls default text sizes
 plt.rc('axes', titlesize=BIGGER_SIZE)     # fontsize of the axes title
 plt.rc('axes', labelsize=BIGGER_SIZE)     # fontsize of the x and y labels
-plt.rc('xtick', labelsize=MEDIUM_SIZE)    # fontsize of the tick labels
-plt.rc('ytick', labelsize=MEDIUM_SIZE)    # fontsize of the tick labels
+plt.rc('xtick', labelsize=BIG_SIZE)    # fontsize of the tick labels
+plt.rc('ytick', labelsize=BIG_SIZE)    # fontsize of the tick labels
 plt.rc('legend', fontsize=BIG_SIZE)       # legend fontsize
 plt.rc('figure', titlesize=BIGGER_SIZE)   # fontsize of the figure title
 
 
 ### Figure 1 ###
-fig, ax = plt.subplots(2, 5, figsize=(25,20), gridspec_kw={'height_ratios': [3, 1]})
+fig, ax = plt.subplots(2, d, figsize=(15, 20), gridspec_kw={'height_ratios': [3, 1]})
 
 floodgate = []
-spf = []
+panin = []
 spf_surrogate = []
 
-# Floodgate, high-quality surrogate
-for fp in os.listdir(DATA_DIR.format('floodgate', '100000')):
-    floodgate.append(np.load(DATA_DIR.format('floodgate', '100000') + fp))
+noise_vals = ["0.5", "0.25", "0.1", "0.01", "0.001", "0.0001", "0.0"]
+
+# Floodgate
+for s in noise_vals:
+    data_dir = DATA_DIR / "floodgate" / s
+    floodgate.append(
+        [np.load(data_dir / fp) for fp in os.listdir(data_dir)]
+    )
 floodgate = np.array(floodgate)
-floodgate_mean = np.mean(floodgate, axis=0)
-floodgate_cov = np.mean((floodgate[:,:,:,0] < gt) * (floodgate[:,:,:,1] > gt), axis=0)
+floodgate_mean = np.mean(floodgate, axis=1)
+floodgate_cov = np.mean((floodgate[:,:,:,0] < gt) * (floodgate[:,:,:,1] > gt), axis=1)
+
+
+mse_vals = [
+    analytical_mse(1 + float(n), 7 + 2 * float(n), 0.1 - 0.5 * float(n)) for n in noise_vals
+]
 
 for i in range(d):
-    ax[0,i].plot(sample_sizes, floodgate_mean[:,i,0], color='green', ls='-', lw=lw, label='Floodgate')
-    ax[0,i].plot(sample_sizes, floodgate_mean[:,i,1], color='green', ls='-', lw=lw)
-    ax[1,i].plot(sample_sizes, floodgate_cov[:,i], color='green', ls='-', lw=lw)
+    ax[0,i].plot(mse_vals, floodgate_mean[:,i,0], color='green', ls='-', lw=lw, label='Floodgate')
+    ax[0,i].plot(mse_vals, floodgate_mean[:,i,1], color='green', ls='-', lw=lw)
+    ax[1,i].plot(mse_vals, floodgate_cov[:,i], color='green', ls='-', lw=lw)
 
-ax[0,2].plot([0], [20], color='grey', ls='--', lw=lw, label='Low-quality $f$')
 
-# Non-surrogate SPF
-for fp in os.listdir(DATA_DIR.format('spf', '.')):
-    spf.append(np.load(DATA_DIR.format('spf', '.') + fp))
-spf = np.array(spf)
-spf_mean = np.mean(spf, axis=0)
-spf_cov = np.mean((spf[:,:,:,0] < gt) * (spf[:,:,:,1] > gt), axis=0)
-
-for i in range(d):
-    ax[0,i].plot(sample_sizes, spf_mean[:,i,0], color='blue', ls='-', lw=lw, label='SPF $f^*$')
-    ax[0,i].plot(sample_sizes, spf_mean[:,i,1], color='blue', ls='-', lw=lw)
-    ax[1,i].plot(sample_sizes, spf_cov[:,i], color='blue', ls='-', lw=lw)
-
-ax[0,2].plot([0], [20], color='grey', ls='-', lw=lw, label='High-quality $f$')
-
-# Surrogate SPF, high-quality surrogate
-for fp in os.listdir(DATA_DIR.format('spf_surrogate', '100000')):
-    spf_surrogate.append(np.load(DATA_DIR.format('spf_surrogate', '100000') + fp))
+# Surrogate SPF
+for s in noise_vals:
+    data_dir = DATA_DIR / "spf_surrogate" / s
+    spf_surrogate.append(
+        [np.load(data_dir / fp) for fp in os.listdir(data_dir)]
+    )
 spf_surrogate = np.array(spf_surrogate)
-spf_surrogate_mean = np.mean(spf_surrogate, axis=0)
-spf_surrogate_cov = np.mean((spf_surrogate[:,:,:,0] < gt) * (spf_surrogate[:,:,:,1] > gt), axis=0)
+spf_surrogate_mean = np.mean(spf_surrogate, axis=1)
+spf_surrogate_cov = np.mean((spf_surrogate[:,:,:,0] < gt) * (spf_surrogate[:,:,:,1] > gt), axis=1)
 
 for i in range(d):
-    ax[0,i].plot(sample_sizes, spf_surrogate_mean[:,i,0], color='orange', ls='-', lw=lw, label='SPF $f$')
-    ax[0,i].plot(sample_sizes, spf_surrogate_mean[:,i,1], color='orange', ls='-', lw=lw)
-    ax[1,i].plot(sample_sizes, spf_surrogate_cov[:,i], color='orange', ls='-', lw=lw)
+    ax[0,i].plot(mse_vals, spf_surrogate_mean[:,i,0], color='orange', ls='-', lw=lw, label='SPF $f$')
+    ax[0,i].plot(mse_vals, spf_surrogate_mean[:,i,1], color='orange', ls='-', lw=lw)
+    ax[1,i].plot(mse_vals, spf_surrogate_cov[:,i], color='orange', ls='-', lw=lw)
 
 
-floodgate = []
-spf_surrogate = []
-
-# Floodgate, low-quality surrogate
-for fp in os.listdir(DATA_DIR.format('floodgate', '10000')):
-    floodgate.append(np.load(DATA_DIR.format('floodgate', '10000') + fp))
-floodgate = np.array(floodgate)
-floodgate_mean = np.mean(floodgate, axis=0)
-floodgate_cov = np.mean((floodgate[:,:,:,0] < gt) * (floodgate[:,:,:,1] > gt), axis=0)
-
-for i in range(d):
-    ax[0,i].plot(sample_sizes, floodgate_mean[:,i,0], color='green', ls='--', lw=lw)
-    ax[0,i].plot(sample_sizes, floodgate_mean[:,i,1], color='green', ls='--', lw=lw)
-    ax[1,i].plot(sample_sizes, floodgate_cov[:,i], color='green', ls='--', lw=lw)
-
-# Surrogate SPF, low-quality surrogate
-for fp in os.listdir(DATA_DIR.format('spf_surrogate', '10000')):
-    spf_surrogate.append(np.load(DATA_DIR.format('spf_surrogate', '10000') + fp))
-spf_surrogate = np.array(spf_surrogate)
-spf_surrogate_mean = np.mean(spf_surrogate, axis=0)
-spf_surrogate_cov = np.mean((spf_surrogate[:,:,:,0] < gt) * (spf_surrogate[:,:,:,1] > gt), axis=0)
+# Panin bounds
+for s in noise_vals:
+    data_dir = DATA_DIR / "panin" / s
+    panin.append(
+        [np.load(data_dir / fp) for fp in os.listdir(data_dir)]
+    )
+panin = np.array(panin)
+panin_mean = np.mean(panin, axis=1)
+panin_cov = np.mean((panin[:,:,:,0] < gt) * (panin[:,:,:,1] > gt), axis=1)
 
 for i in range(d):
-    ax[0,i].plot(sample_sizes, spf_surrogate_mean[:,i,0], color='orange', ls='--', lw=lw)
-    ax[0,i].plot(sample_sizes, spf_surrogate_mean[:,i,1], color='orange', ls='--', lw=lw)
-    ax[1,i].plot(sample_sizes, spf_surrogate_cov[:,i], color='orange', ls='--', lw=lw)
+    ax[0,i].plot(mse_vals, panin_mean[:,i,0], color="purple", ls='-', lw=lw, label="Panin Bounds")
+    ax[0,i].plot(mse_vals, panin_mean[:,i,1], color="purple", ls='-', lw=lw)
+    ax[1,i].plot(mse_vals, panin_cov[:,i], color="purple", ls='-', lw=lw)
 
 
-
-x_ticks = [100, 1000, 10000, 100000]
-x_tick_labs = ['100', '1000', '10000', '100000']
 for i in range(d):
-    ax[0,i].set(xscale='log', title=X_labels[i], ylim=(-.01,1))
-    ax[1,i].set(xticks=x_ticks, xscale='log')
+    ax[0,i].set(xscale='log', title=f"X_i", ylim=(-.01,1))
+    ax[1,i].set(xscale='log')
     ax[0,i].axhline(y=gt[i], color='red', ls=':', lw=lw, label='Target')
     ax[1,i].axhline(y=0.95, color='red', ls=':', lw=lw)
     ax[1,i].set_ylim((0,1.05))
@@ -127,112 +106,16 @@ for i in range(d):
         ax[0,i].set_yticks([])
         ax[1,i].set_yticks([])
     ax[0,i].set_xticks([])
+    ax[0,i].invert_xaxis()
+    ax[1,i].invert_xaxis()
 
 
 ax[0,0].set(ylabel='Confidence Bounds for $S_j$')
 ax[1,0].set(ylabel='Coverage')
-ax[1,2].set_xlabel('Computational Budget $N$', fontsize=HUGE_SIZE)
-ax[0,2].legend(loc='lower center', bbox_to_anchor=(0.5, -.6), ncol=3, fancybox=True)
-ax[1,2].xaxis.labelpad = 15
+ax[1,1].set_xlabel('MSE(f)', fontsize=HUGE_SIZE)
+ax[0,1].legend(loc='lower center', bbox_to_anchor=(0.5, -.6), ncol=2, fancybox=True)
+ax[1,1].xaxis.labelpad = 15
 fig.subplots_adjust(wspace=0.045, hspace=0.025)
 
 # Save plots
-plt.savefig(FIG_DIR + 'Hymod_bounds.png', bbox_inches="tight")
-
-
-
-
-
-### Figure 2 ###
-# Plot formatting
-plt.rc('font', size=SMALL_SIZE)          # controls default text sizes
-plt.rc('axes', titlesize=BIGGER_SIZE)     # fontsize of the axes title
-plt.rc('axes', labelsize=BIGGER_SIZE)    # fontsize of the x and y labels
-plt.rc('xtick', labelsize=MEDIUM_SIZE)    # fontsize of the tick labels
-plt.rc('ytick', labelsize=MEDIUM_SIZE)    # fontsize of the tick labels
-plt.rc('legend', fontsize=BIG_SIZE)    # legend fontsize
-plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
-
-
-fig, ax = plt.subplots(2, 5, figsize=(25,10))
-
-floodgate = []
-panin = []
-
-# Floodgate, high-quality surrogate
-for fp in os.listdir(DATA_DIR.format('floodgate', 100000)):
-    floodgate.append(np.load(DATA_DIR.format('floodgate', '100000') + fp))
-floodgate = np.array(floodgate)
-floodgate_mean = np.mean(floodgate[:,:,:,1] - floodgate[:,:,:,0], axis=0)
-floodgate_cov = np.mean((floodgate[:,:,:,0] < gt) * (floodgate[:,:,:,1] > gt), axis=0)
-
-for i in range(d):
-    ax[0,i].plot(sample_sizes, floodgate_mean[:,i], color='green', ls='-', lw=lw, label='Floodgate')
-    ax[1,i].plot(sample_sizes, floodgate_cov[:,i], color='green', ls='-', lw=lw)
-
-ax[0,2].plot([0], [20], color='grey', ls='--', lw=lw, label='Low-quality $f$')
-
-# Panin bound, high-quality surrogate
-for fp in os.listdir(DATA_DIR.format('panin', 100000)):
-    panin.append(np.load(DATA_DIR.format('panin', '100000') + fp))
-panin = np.array(panin)
-panin_mean = np.mean(panin[:,:,:,1] - panin[:,:,:,0], axis=0)
-panin_cov = np.mean((panin[:,:,:,0] < gt) * (panin[:,:,:,1] > gt), axis=0)
-
-for i in range(d):
-    ax[0,i].plot(sample_sizes, panin_mean[:,i], color='purple', ls='-', lw=lw, label='Panin Bounds')
-    ax[1,i].plot(sample_sizes, panin_cov[:,i], color='purple', ls='-', lw=lw)
-
-ax[0,2].plot([0], [20], color='grey', ls='-', lw=lw, label='High-quality $f$')
-
-
-floodgate = []
-panin = []
-
-# Floodgate, low-quality surrogate
-for fp in os.listdir(DATA_DIR.format('floodgate', 10000)):
-    floodgate.append(np.load(DATA_DIR.format('floodgate', '10000') + fp))
-floodgate = np.array(floodgate)
-floodgate_mean = np.mean(floodgate[:,:,:,1] - floodgate[:,:,:,0], axis=0)
-floodgate_cov = np.mean((floodgate[:,:,:,0] < gt) * (floodgate[:,:,:,1] > gt), axis=0)
-
-for i in range(d):
-    ax[0,i].plot(sample_sizes, floodgate_mean[:,i], color='green', ls='--', lw=lw)
-    ax[1,i].plot(sample_sizes, floodgate_cov[:,i], color='green', ls='--', lw=lw)
-
-# Panin bound, low-quality surrogate
-for fp in os.listdir(DATA_DIR.format('panin', 10000)):
-    panin.append(np.load(DATA_DIR.format('panin', '10000') + fp))
-panin = np.array(panin)
-panin_mean = np.mean(panin[:,:,:,1] - panin[:,:,:,0], axis=0)
-panin_cov = np.mean((panin[:,:,:,0] < gt) * (panin[:,:,:,1] > gt), axis=0)
-
-for i in range(d):
-    ax[0,i].plot(sample_sizes, panin_mean[:,i], color='purple', ls='--', lw=lw)
-    ax[1,i].plot(sample_sizes, panin_cov[:,i], color='purple', ls='--', lw=lw)
-
-ax[0,2].plot([0], [20], color='red', ls=':', lw=lw, label="Target")
-
-
-# Plot formatting
-for i in range(d):
-    ax[0,i].set(xscale='log', title=X_labels[i])
-    ax[1,i].set(xticks=x_ticks, xscale='log')   
-    ax[1,i].axhline(y=0.95, color='red', ls=':', lw=lw)
-    ax[0,i].set_ylim((0,1.01))
-    ax[1,i].set_ylim((0,1.05))
-    if i > 0:
-        ax[0,i].set_yticks([])
-        ax[1,i].set_yticks([])
-    ax[0,i].set_xticks([])
-
-ax[0,0].set(ylabel='Width of \nConfidence Intervals')
-ax[1,0].set(ylabel='Coverage')
-ax[1,2].set_xlabel('Computational Budget $N$', fontsize=HUGE_SIZE)
-ax[0,2].legend(loc='lower center', bbox_to_anchor=(0.5, -1.7), ncol=3, fancybox=True)
-ax[1,2].xaxis.labelpad = 15
-fig.subplots_adjust(wspace=0.04, hspace=0.04)
-
-# Save plots
-plt.savefig(FIG_DIR + 'Hymod_panin_widths.png', bbox_inches="tight")
-
+plt.savefig(FIG_DIR / 'Ishigami_bounds.png', bbox_inches="tight")
